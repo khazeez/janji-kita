169345ff43f84e7d15bc0a4ae783d896085ff27a
Khoirul Aziz <cakirul211001@gmail.com>
2025-12-17 11:30:10 +0700

    update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -241,9 +241,9 @@ export default function Dashboard() {
   // Jika tidak ada user (fallback)
   if (!user) {
     return (
-      <div className="text-white text-2xl">
+      <div className="text-white text-center text-2xl">
         User tidak ada
-        User: {user}
+        {user}
       </div>
     );
   }
